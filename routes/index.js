4171e3b4556f4bff4c9fa140fed862eea885a758
jaab30 <jaab30@yahoo.com>
2020-07-07 19:49:23 -0400

    added catch all route to routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const path = require("path");
 
 // Routes for authentication
 router.use("/auth", authRoutes);
+
+// If no API routes are hit, send the React app
+router.use("*", (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
 
 module.exports = router;
